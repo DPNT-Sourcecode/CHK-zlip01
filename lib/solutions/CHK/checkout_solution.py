243b23083c916aa712f57bfb9e78d7e0cc93f135
sbx_user1051 <sbx_user1051@169.254.35.205>
2023-11-15 10:06:19 +0000

    Wed Nov 15 10:06:19 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,45 +1,62 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
+
+class Basket:
+    def __init__(self, sku_items: str):
+        self.offers = {
+            "A" : [(5, 200), ]
+        }
+        self.basket_value = self._calulateBasketValue(sku_items)
+
+    def _calulateBasketValue(self, sku_items):
+        if sku_items == "":
+            return 0
+
+        valid_sku_identifiers = {'A', 'B', 'C', 'D'}
+
+        # Get the frequency of all the unique SKU identifiers
+        # in the basket, to be able to calculate the total
+        # basket value efficiently
+
+        basket_map = {}
+
+        for sku_identifier in sku_items:
+            if sku_identifier not in valid_sku_identifiers:
+                return -1
+            if sku_identifier in basket_map:
+                basket_map[sku_identifier] += 1
+            else:
+                basket_map[sku_identifier] = 1
+
+        # Maps the SKU identifiers to their values
+        value_map = {
+            "A": 50,
+            "B": 30,
+            "C": 20,
+            "D": 15
+        }
+
+        basket_total = 0
+
+        for sku_identifier, frequency in basket_map.items():
+            if sku_identifier == "A":
+                if frequency >= 3:
+                    basket_total += (frequency // 3) * 130
+                    frequency = frequency % 3
+            elif sku_identifier == "B":
+                if frequency >= 2:
+                    basket_total += (frequency // 2) * 45
+                    frequency = frequency % 2
+
+            basket_total += frequency * value_map[sku_identifier]
+
+        return basket_total
+
+    def getValue(self):
+        return self.basket_value
+
+
 def checkout(sku_string: str) -> int:
-    if sku_string == "":
-        return 0
+    basket = Basket(sku_string)
+    return basket.getValue()
 
-    valid_sku_identifiers = {'A', 'B', 'C', 'D'}
-
-    # Get the frequency of all the unique SKU identifiers
-    # in the basket, to be able to calculate the total
-    # basket value efficiently 
-
-    basket_map = {}
-
-    for sku_identifier in sku_string:
-        if sku_identifier not in valid_sku_identifiers:
-            return -1
-        if sku_identifier in basket_map:
-            basket_map[sku_identifier] += 1
-        else:
-            basket_map[sku_identifier] = 1
-
-    # Maps the SKU identifiers to their values
-    value_map = {
-        "A": 50,
-        "B": 30,
-        "C": 20,
-        "D": 15
-    }
-
-    basket_total = 0
-
-    for sku_identifier, frequency in basket_map.items():
-        if sku_identifier == "A":
-            if frequency >= 3:
-                basket_total += (frequency // 3) * 130
-                frequency = frequency % 3
-        elif sku_identifier == "B":
-            if frequency >= 2:
-                basket_total += (frequency // 2) * 45
-                frequency = frequency % 2
-
-        basket_total += frequency * value_map[sku_identifier]
-
-    return basket_total
 

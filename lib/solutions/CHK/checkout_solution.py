d51fa6fd9301cb4a1afcb6e179f0005695ff55b6
sbx_user1051 <sbx_user1051@169.254.35.205>
2023-11-15 09:25:10 +0000

    Wed Nov 15 09:25:10 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,4 +1,9 @@
 def checkout(sku_string: str) -> int:
+    
+    # Get the frequency of all the unique SKU identifiers
+    # in the basket, to be able to calculate the total
+    # basket value efficiently 
+
     basket_map = {}
     for sku in sku_string:
         if sku in basket_map:
@@ -6,4 +11,13 @@ def checkout(sku_string: str) -> int:
         else:
             basket_map[sku] = 1
     
+    # Maps the SKU identifiers to their values
+
+    value_map = {}
+    
+
+    basket_total = 0
+
+    for sku_identifier, frequency in basket_map.items():
+        if sku_identifier != "A" and sk
 

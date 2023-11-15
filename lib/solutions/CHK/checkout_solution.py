0aad4bf377679688fc078b3be5ec3ad6644198c2
sbx_user1051 <sbx_user1051@169.254.35.205>
2023-11-15 09:22:10 +0000

    Wed Nov 15 09:22:10 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,9 @@
+def checkout(sku_string: str) -> int:
+    basket_map = {}
+    for sku in sku_string:
+        if sku in basket_map:
+            basket_map[sku] += 1
+        else:
+            basket_map[sku] = 1
+    
 
-
-# noinspection PyUnusedLocal
-# skus = unicode string
-def checkout(skus):
-    raise NotImplementedError()

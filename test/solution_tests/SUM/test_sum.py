b1e88e2967b3f9b6b9eb8c5843efe8a2923d9153
sbx_user1051 <sbx_user1051@169.254.35.205>
2023-11-15 08:56:12 +0000

    Wed Nov 15 08:56:12 UTC 2023

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -4,9 +4,10 @@ from solutions.SUM.sum_solution import compute
 class TestSum(TestCase):
     def test_sum_one(self):
         self.assertEqual(compute(10, 10), 20)
-    def test_sum_one(self):
-        self.assertEqual(compute(10, 10), 20)
-    def test_sum_one(self):
-        self.assertEqual(compute(10, 10), 20)
+    def test_sum_two(self):
+        self.assertEqual(compute(0, 0), 0)
+    def test_sum_three(self):
+        self.assertEqual(compute(100, 100), 200)
+
 
 

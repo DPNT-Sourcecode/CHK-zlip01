36856317580e1f1050853c313bcef9074a43f9a0
sbx_user1051 <sbx_user1051@169.254.35.205>
2023-11-15 11:27:51 +0000

    Wed Nov 15 11:27:51 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,14 +3,19 @@
 class Basket:
     def __init__(self, sku_items: str):
         self.sku_values = {
-            "A": 50,
-            "B": 30,
-            "C": 20,
-            "D": 15,
-            "E": 40,
-            "F": 10,
-            "G": 20,
-            "H": 10,
+            "A": 50, "B": 30,
+            "C": 20, "D": 15,
+            "E": 40, "F": 10,
+            "G": 20, "H": 10,
+            "I": 35, "J": 60,
+            "K": 80, "L": 90,
+            "M": 15, "N": 40,
+            "O": 10, "P": 50,
+            "Q": 30, "R": 50,
+            "S": 30, "T": 20,
+            "U": 40, "V": 50,
+            "W": 20, "X": 90,
+            "Y": 10, "Z": 50
         }
 
         self.sku_offers = {
@@ -102,4 +107,5 @@ class Basket:
 def checkout(sku_string: str) -> int:
     basket = Basket(sku_string)
     return basket.getValue()
+
 
